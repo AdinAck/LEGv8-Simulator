3ef942b5c91eed37a47c736952240dd9037ca9ef
Adin Ackerman <adinackerman@gmail.com>
2022-09-25 10:14:46 -0700

    Initial Commit

---
+++ LEGv8-Simulator/LEGv8_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEGv8_SimulatorApp.swift
+//  LEGv8-Simulator
+//
+//  Created by Adin Ackerman on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LEGv8_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
